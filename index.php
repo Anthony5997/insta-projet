1cc77325f6b83914094299bd69cc73659568faf4
Anthony5997 <anthony.rita@outlook.com>
2021-03-15 11:25:53 +0100

    ajout header/footer

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include("partials/header.php");
+
+echo "ok let's go";
+include("partials/footer.php");
---
+++ partials/header.php
@@ -0,0 +1,42 @@
+<?php   session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <title>Test NavBar de connexion</title>
+</head>
+<nav>
+    <div class="topnav">
+    <a class="" href="index.php">Accueil</a>
+    <a class="" href="form-inscription.php">Inscrivez vous</a>
+    <br>
+    <?php  if(empty($_SESSION["connect"])){
+    echo '<div class="login-container">
+        <form action="process/connexion-process.php" method="post">
+            <input type="text" placeholder="username" name="mailVerif">
+            <input type="text" placeholder="password" name="passVerif">
+        <button type="submit">Connexion</button>
+        </form>';
+    }else{
+        echo "<div class='session-align'>
+                <p class="."button-perso".">Salut ". $_SESSION["user"]."</p> 
+                <a href="."process/deco.php".">Déconnexion</a>";
+                ?>
+            </div>
+   <?php } ?>
+    </div>
+    </div>
+<?php if(isset($_GET["message"])){
+        echo '<div style="padding: 10px; width:25vw; background:green; color:#fff;">
+                '.$_GET["message"].'
+             </div>';
+    }
+ ?>
+ </nav>
+<body>
