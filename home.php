de1603a06e792aa81cdb275d1ee500ae896f84c2
Anthony5997 <anthony.rita@outlook.com>
2021-05-12 09:33:29 +0200

    Add SASS and some modif in the home page

--- home.php
+++ home.php
@@ -12,6 +12,44 @@ $_SESSION['user'] = $currentUser;
 include("partials/header.php");
 var_dump($_SESSION);
 ?>
+<div class="container">
+  <div class="row">
+    <div class="col-12 sm- button">
+      <h6>Suggestion pour vous
+        <button id="togg1"><img src="https://img.icons8.com/officexs/16/000000/expand-arrow.png" /></button>
+      </h6>
+      <!-- <button id="togg2">Cliquez-moi !</button> -->
+      <div class="container cardprofile" id="d1">
+        <div class="row">
+          <div class="col-md-6 col-lg-4">
+            <div class="card">
+              <div class="img">
+                <img src="/insta-projet/assets/img/paysage-montagne-photographe-en-herbe-1024x576.jpg">
+              </div>
+              <div class="info">
+                <span class="name"> <?= $_SESSION["user"]->getName() ?></span>
+                <span class="id">@<?= $_SESSION["user"]->getName() ?></span>
+                <button class="button2">Follow</button>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <div class="card">
+              <div class="img">
+                <img src="/insta-projet/assets/img/paysage-montagne-photographe-en-herbe-1024x576.jpg">
+              </div>
+              <div class="info">
+                <span class="name"> <?= $_SESSION["user"]->getName() ?></span>
+                <span class="id">@<?= $_SESSION["user"]->getName() ?></span>
+                <button class="button2">Follow</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <div class="container">
   <div class="row">
--- partials/footer.php
+++ partials/footer.php
@@ -4,6 +4,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="/insta-projet/assets/js/profilPictureScript.js"></script>
     <script src="/insta-projet/assets/js/script.js"></script>
+    <script src="/insta-projet/assets/js/divcache.js"></script>
     <script src="/insta-projet/assets/js/ajax-modal.js"></script>
     <script src="/insta-projet/assets/js/search-asynch.js"></script>
     </body>
